d2655baed821b64bef78e35d0089d11af706640f
Abdul Ismail <79646763+abdiulismail@users.noreply.github.com>
2023-07-19 20:11:04 +0300

    chunk the list at every 3 element

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -5,10 +5,24 @@ defmodule Identicon do
     input
     |> hash_input()
     |> pick_color()
+    |> buil_grid()
 
   end
 
-  # def pick_color(myimage) do
+
+
+  def hash_input(input) do
+    # hash = :crypto.hash(:md5, "banana")
+    # :binary.bin_to_list(hash)
+
+    hex = :crypto.hash(:md5,input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+
+  end
+
+    # def pick_color(myimage) do
   #   # %Identicon.Image{hex: hexlist} = myimage
   #   # [r,g,b | _tail ] = hexlist
   #   %Identicon.Image{hex: [r,g,b | _tail ]} = myimage
@@ -22,15 +36,9 @@ defmodule Identicon do
     %Identicon.Image{myimage | color: {r,g,b}}
   end
 
-  def hash_input(input) do
-    # hash = :crypto.hash(:md5, "banana")
-    # :binary.bin_to_list(hash)
-
-    hex = :crypto.hash(:md5,input)
-    |> :binary.bin_to_list
-
-    %Identicon.Image{hex: hex}
+  def buil_grid(%Identicon.Image{hex: hex} = myimage) do
+    hex
+    |> Enum.chunk_every(3)
 
   end
-
 end
